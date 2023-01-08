6b91606c6813ca9f1d045908aab57b68acd9daa5
japanesebonobo <tomi119119@gmail.com>
2023-01-09 04:35:12 +0900

    add chapter8

--- src/chapter4/ClamPizza.java
+++ src/chapter4/ClamPizza.java
@@ -9,9 +9,5 @@ public class ClamPizza extends Pizza {
     
     void prepare() {
         System.out.println("Preparing " + name);
-        dough = ingredientFactory.createDough();
-        sauce = ingredientFactory.createSauce();
-        cheese = ingredientFactory.createCheese();
-        clam = ingredientFactory.createClam();
     }
 }
---
+++ src/chapter8/CaffeineBeverage.java
@@ -0,0 +1,28 @@
+package chapter8;
+
+public abstract class CaffeineBeverage {
+    
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if (customerWantsCondiments()) {
+            addCondiments();
+        }
+    }
+
+    abstract void brew();
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling wateer");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    boolean customerWantsCondiments() {
+        return true;
+    }
+}
