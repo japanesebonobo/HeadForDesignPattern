304f0710a09f969bbed178fbdb612b9ab5b3d9c0
japanesebonobo <tomi119119@gmail.com>
2023-05-04 23:36:07 +0900

    10章

---
+++ src/chapter10/State.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public interface State {
+    
+    public void insertQuarter();
+    public void ejectQuarter();
+    public void turnCrank();
+    public void dispense();
+
+    public void refiil();
+}
