ddc8831eccb312569d4d5bcd749e5abb2958e6a6
japanesebonobo <tomi119119@gmail.com>
2023-05-05 23:52:39 +0900

    11章

---
+++ src/chapter11/remoteProxy/State.java
@@ -0,0 +1,11 @@
+package chapter11.remoteProxy;
+
+public interface State {
+    
+    public void insertQuarter();
+    public void ejectQuarter();
+    public void turnCrank();
+    public void dispense();
+
+    public void refiil();
+}
